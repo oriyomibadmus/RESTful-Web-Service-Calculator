625a073aa459b098a4e01fe41e9e4c18a1767c57
Badmus Oriyomi <master.yummy123@gmail.com>
2023-11-03 13:21:24 +0100

    Calculator file

---
+++ hello/src/main/java/com/practicalcodes/hello/calculator.java
@@ -0,0 +1,20 @@
+package com.practicalcodes.hello;
+
+
+public class calculator {
+    public double add(double op1, double op2) {
+        return op1 + op2;
+    }
+
+    public double subtract(double op1, double op2) {
+        return op1 - op2;
+    }
+
+    public double multiply(double op1, double op2) {
+        return op1 * op2;
+    }
+
+    public double divide(double op1, double op2) {
+        return op1 / op2;
+    }
+}
